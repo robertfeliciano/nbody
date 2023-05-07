4d2253ea226426673f646094cb539a7bc0f182a5
robertfeliciano <robertfeliciano314@gmail.com>
2023-05-07 19:03:11 -0400

    more graphs

---
+++ scripts/fast_v_slow.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import subprocess
+import re
+import signal
+import numpy as np
+import pandas as pd
+
+def run(n: int) -> float:
+    '''
+    Runs the nbody simulation with n bodies for 10 iterations.
+    Returns the average time a time iteration took to compute.
+    '''
+    output = subprocess.run(f"../bin/nbody {n} 10", shell=True, capture_output=True).stdout.decode('utf-8')
+    times = np.array([float(t) for t in re.findall(r'\d+\.\d\d', output)])
+    avg = round(np.average(times), 3)
+    # progress indicator 
+    print(f"average for {n} bodies is {avg} ms.")
+    return avg
+
+def time(version: str) -> list[float]:
+    '''
+    Times the version specified by `version` for 5 different body configurations
+    '''
+    subprocess.run(f"make {version}", shell=True)
+    avg1k = run(1000)
+    avg10k = run(10000)
+    avg50k = run(50000)
+    avg100k = run(100000)
+    return [avg1k, avg10k, avg50k, avg100k]
+
+def main() -> None:
+    versions = ['cuda', 'default']
+    df = pd.DataFrame(columns=versions, 
+                      index=['1K', '10K', '50K', '100K'])
+    
+    for v in versions:
+        df[v] = time(v)
+        # i just need to see some progress indicator lol
+        print(df.head())
+        df.to_csv('../data/fastslow_backup.csv', index=False)
+
+    df.rename(columns={'cuda':'cuda', 'default':'sequential cpu'}, inplace=True)
+    fig = df.plot(figsize=(10,10)).get_figure()
+    fig.savefig('../graphs/fastslow_graph.pdf')
+
+
+if __name__ == "__main__":
+    main()
